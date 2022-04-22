9d7295e5bf325b4bad168dbec510442b66c93693
Chithian <chithiansamony@gmail.com>
2022-04-23 00:17:20 +0700

    - Make Link Button to open Url Apple watch website
    - Custom style layer button (Start Paring)

---
+++ Watch AppKh/MyWatchViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Watch AppKh
+//
+//  Created by Chithian on 22/4/22.
+//
+
+import UIKit
+
+class MyWatchViewController: UIViewController {
+
+    @IBOutlet weak var startParingBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        //Start Paring Button style
+        startParingBtn.layer.cornerRadius = 10
+        
+    }   
+
+    //Link Button
+    @IBAction func linkButton(_ sender: UIButton) {
+        if let url = NSURL(string: "https://www.apple.com/watch/"){
+            UIApplication.shared.openURL(url as URL)
+            }
+    }
+    
+}
+
